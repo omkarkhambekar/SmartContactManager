eada966c1c06b6cd2c386cf855246d56d022af66
omkarkhambekar <khambekaromkar123@gmail.com>
2024-07-09 23:47:28 +0530

    added form login,authorize http requests through spring http security

--- src/main/java/com/omkar_smartcontactmanager/scm/config/SecurityConfig.java
+++ src/main/java/com/omkar_smartcontactmanager/scm/config/SecurityConfig.java
@@ -2,6 +2,9 @@ package com.omkar_smartcontactmanager.scm.config;
 
 import com.omkar_smartcontactmanager.scm.services.SecurityCustomUserDetailService;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +12,16 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
@@ -59,7 +69,35 @@ public class SecurityConfig {
             authorize.anyRequest().permitAll();
         });
 
-        httpSecurity.formLogin(Customizer.withDefaults());
+        httpSecurity.formLogin(formLogin -> {
+
+            //
+            formLogin.loginPage("/login");
+            formLogin.loginProcessingUrl("/authenticate");
+            formLogin.successForwardUrl("/user/dashboard");
+//            formLogin.failureForwardUrl("/login?error=true");
+            formLogin.usernameParameter("email");
+            formLogin.passwordParameter("password");
+//            formLogin.failureHandler(new AuthenticationFailureHandler() {
+//                @Override
+//                public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//
+//                }
+//            });
+//            formLogin.successHandler(new AuthenticationSuccessHandler() {
+//                @Override
+//                public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//
+//                }
+//            });
+        });
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+
+        httpSecurity.logout(logoutForm ->{
+            logoutForm.logoutUrl("/logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
 
         return httpSecurity.build();
     }
